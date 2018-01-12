96a3920bd28a563740732cd7267a803fd3a632c9
oldbay <old_bay@mail.ru>
2018-01-12 12:56:04 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='raster_tools',
     version='0.4',
     packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README')).read(),
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
     install_requires=[
         'GDAL',
         'numpy'
